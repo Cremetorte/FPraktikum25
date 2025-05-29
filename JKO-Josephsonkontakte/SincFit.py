bfee2d301562a8278a7f136c1b3039612f49e037
cremetorte <deschwab@t-online.de>
2025-05-29 11:14:58 +0200

    Sync?

--- JKO-Josephsonkontakte/SincFit.py
+++ JKO-Josephsonkontakte/SincFit.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
-import matplotlib
-matplotlib.use('TkAgg')
+# import matplotlib
+# matplotlib.use('TkAgg')
 
 
 
